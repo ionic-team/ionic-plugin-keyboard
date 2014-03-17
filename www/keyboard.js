0e4df70862b86128e98188d30b923c0a7ce5aa8b
Tim Lancina <tlancina@drifty.com>
2014-03-17 15:47:04 -0500

    fix keyboard.js

--- www/keyboard.js
+++ www/keyboard.js
@@ -1,4 +1,4 @@
-cordova.define("org.apache.cordova.keyboard.keyboard", function(require, exports, module) {/*
+/*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -43,5 +43,6 @@ Keyboard.disableScroll = function() {
 };
 
 module.exports = Keyboard;
-});
+
+
 
