04b027248179d419af1383221a9c17e7fc3b3567
Tim Lancina <tim@drifty.com>
2014-05-20 11:54:34 -0500

    update keyboard.js

--- www/keyboard.js
+++ www/keyboard.js
@@ -18,9 +18,11 @@ Keyboard.disableScroll = function(disable) {
  exec(null, null, "Keyboard", "disableScroll", [disable]);
 };
 
+/*
 Keyboard.styleDark = function(dark) {
  exec(null, null, "Keyboard", "styleDark", [dark]);
 };
+*/
 
 Keyboard.isVisible = false; 
 
