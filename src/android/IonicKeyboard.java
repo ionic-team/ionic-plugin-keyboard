cff381d5068f8050b4966f5bf4b3cd2e8c986c04
Bas van Beek <bas@tobin.nl>
2014-08-03 16:55:31 +0200

    Added Keyboard.show function for Android. This can be used to force the soft keyboard to appear if an auto focus on a form text element does not.

--- src/android/IonicKeyboard.java
+++ src/android/IonicKeyboard.java
@@ -1,4 +1,4 @@
-package com.ionic.keyboard; 
+package com.ionic.keyboard;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
@@ -19,22 +19,22 @@ public class IonicKeyboard extends CordovaPlugin{
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-        
+
         //calculate density-independent pixels (dp)
         //http://developer.android.com/guide/practices/screens_support.html
         DisplayMetrics dm = new DisplayMetrics();
         cordova.getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
         final float density = dm.density;
-        
+
         final CordovaWebView appView = webView;
-        
+
         //http://stackoverflow.com/a/4737265/1091751 detect if keyboard is showing
         final View rootView = cordova.getActivity().getWindow().getDecorView().findViewById(android.R.id.content).getRootView();
         OnGlobalLayoutListener list = new OnGlobalLayoutListener() {
             int previousHeightDiff = 0;
             @Override
             public void onGlobalLayout() {
-            	Rect r = new Rect();
+                Rect r = new Rect();
                 //r will be populated with the coordinates of your view that area still visible.
                 rootView.getWindowVisibleDisplayFrame(r);
 
@@ -42,10 +42,10 @@ public class IonicKeyboard extends CordovaPlugin{
                 int pixelHeightDiff = (int)(heightDiff / density);
                 if (pixelHeightDiff > 100 && pixelHeightDiff != previousHeightDiff) { // if more than 100 pixels, its probably a keyboard...
                     appView.sendJavascript("cordova.plugins.Keyboard.isVisible = true");
-                    appView.sendJavascript("cordova.fireWindowEvent('native.keyboardshow', { 'keyboardHeight':" + Integer.toString(pixelHeightDiff)+"});");        
+                    appView.sendJavascript("cordova.fireWindowEvent('native.keyboardshow', { 'keyboardHeight':" + Integer.toString(pixelHeightDiff)+"});");
 
                     //deprecated
-                    appView.sendJavascript("cordova.fireWindowEvent('native.showkeyboard', { 'keyboardHeight':" + Integer.toString(pixelHeightDiff)+"});");                   
+                    appView.sendJavascript("cordova.fireWindowEvent('native.showkeyboard', { 'keyboardHeight':" + Integer.toString(pixelHeightDiff)+"});");
                 }
                 else if ( pixelHeightDiff != previousHeightDiff && ( previousHeightDiff - pixelHeightDiff ) > 100 ){
                     appView.sendJavascript("cordova.plugins.Keyboard.isVisible = false");
@@ -56,23 +56,32 @@ public class IonicKeyboard extends CordovaPlugin{
                 }
                 previousHeightDiff = pixelHeightDiff;
              }
-        }; 
-        
+        };
+
         rootView.getViewTreeObserver().addOnGlobalLayoutListener(list);
     }
-	
+
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if ("close".equals(action)) {
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
-                	//http://stackoverflow.com/a/7696791/1091751
-                	InputMethodManager inputManager = (InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-                	View v = cordova.getActivity().getCurrentFocus();
-                	
-                	if (v == null) {
-                		callbackContext.error("No current focus");
-                	}
-                	inputManager.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                    //http://stackoverflow.com/a/7696791/1091751
+                    InputMethodManager inputManager = (InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    View v = cordova.getActivity().getCurrentFocus();
+
+                    if (v == null) {
+                        callbackContext.error("No current focus");
+                    }
+                    inputManager.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                    callbackContext.success(); // Thread-safe.
+                }
+            });
+            return true;
+        }
+        if ("show".equals(action)) {
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    ((InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(0, InputMethodManager.HIDE_IMPLICIT_ONLY);
                     callbackContext.success(); // Thread-safe.
                 }
             });
@@ -80,7 +89,7 @@ public class IonicKeyboard extends CordovaPlugin{
         }
         return false;  // Returning false results in a "MethodNotFound" error.
     }
-	
+
 
 }
 
