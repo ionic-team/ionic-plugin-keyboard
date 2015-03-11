0a1165af7d4aa248ddbd218b60a96961090d2eb4
Tim Lancina <tim@drifty.com>
2015-03-11 11:00:36 -0500

    don't use sendJavascript

--- src/android/IonicKeyboard.java
+++ src/android/IonicKeyboard.java
@@ -4,6 +4,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,50 +16,10 @@ import android.view.View;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.view.inputmethod.InputMethodManager;
 
-public class IonicKeyboard extends CordovaPlugin{
+public class IonicKeyboard extends CordovaPlugin {
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-
-        //calculate density-independent pixels (dp)
-        //http://developer.android.com/guide/practices/screens_support.html
-        DisplayMetrics dm = new DisplayMetrics();
-        cordova.getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
-        final float density = dm.density;
-
-        final CordovaWebView appView = webView;
-
-        //http://stackoverflow.com/a/4737265/1091751 detect if keyboard is showing
-        final View rootView = cordova.getActivity().getWindow().getDecorView().findViewById(android.R.id.content).getRootView();
-        OnGlobalLayoutListener list = new OnGlobalLayoutListener() {
-            int previousHeightDiff = 0;
-            @Override
-            public void onGlobalLayout() {
-                Rect r = new Rect();
-                //r will be populated with the coordinates of your view that area still visible.
-                rootView.getWindowVisibleDisplayFrame(r);
-
-                int heightDiff = rootView.getRootView().getHeight() - (r.bottom);
-                int pixelHeightDiff = (int)(heightDiff / density);
-                if (pixelHeightDiff > 100 && pixelHeightDiff != previousHeightDiff) { // if more than 100 pixels, its probably a keyboard...
-                    appView.sendJavascript("cordova.plugins.Keyboard.isVisible = true");
-                    appView.sendJavascript("cordova.fireWindowEvent('native.keyboardshow', { 'keyboardHeight':" + Integer.toString(pixelHeightDiff)+"});");
-
-                    //deprecated
-                    appView.sendJavascript("cordova.fireWindowEvent('native.showkeyboard', { 'keyboardHeight':" + Integer.toString(pixelHeightDiff)+"});");
-                }
-                else if ( pixelHeightDiff != previousHeightDiff && ( previousHeightDiff - pixelHeightDiff ) > 100 ){
-                    appView.sendJavascript("cordova.plugins.Keyboard.isVisible = false");
-                    appView.sendJavascript("cordova.fireWindowEvent('native.keyboardhide')");
-
-                    //deprecated
-                    appView.sendJavascript("cordova.fireWindowEvent('native.hidekeyboard')");
-                }
-                previousHeightDiff = pixelHeightDiff;
-             }
-        };
-
-        rootView.getViewTreeObserver().addOnGlobalLayoutListener(list);
     }
 
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
@@ -88,9 +49,59 @@ public class IonicKeyboard extends CordovaPlugin{
             });
             return true;
         }
+        if ("init".equals(action)) {
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                	//calculate density-independent pixels (dp)
+                    //http://developer.android.com/guide/practices/screens_support.html
+                    DisplayMetrics dm = new DisplayMetrics();
+                    cordova.getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
+                    final float density = dm.density;
+
+                    //http://stackoverflow.com/a/4737265/1091751 detect if keyboard is showing
+                    final View rootView = cordova.getActivity().getWindow().getDecorView().findViewById(android.R.id.content).getRootView();
+                    OnGlobalLayoutListener list = new OnGlobalLayoutListener() {
+                        int previousHeightDiff = 0;
+                        @Override
+                        public void onGlobalLayout() {
+                            Rect r = new Rect();
+                            //r will be populated with the coordinates of your view that area still visible.
+                            rootView.getWindowVisibleDisplayFrame(r);
+                            
+                            PluginResult result;
+
+                            int heightDiff = rootView.getRootView().getHeight() - r.bottom;
+                            int pixelHeightDiff = (int)(heightDiff / density);
+                            if (pixelHeightDiff > 100 && pixelHeightDiff != previousHeightDiff) { // if more than 100 pixels, its probably a keyboard...
+                            	String msg = "S" + Integer.toString(pixelHeightDiff);
+                                result = new PluginResult(PluginResult.Status.OK, msg);
+                                result.setKeepCallback(true);
+                                callbackContext.sendPluginResult(result);
+                            }
+                            else if ( pixelHeightDiff != previousHeightDiff && ( previousHeightDiff - pixelHeightDiff ) > 100 ){
+                            	String msg = "H";
+                                result = new PluginResult(PluginResult.Status.OK, msg);
+                                result.setKeepCallback(true);
+                                callbackContext.sendPluginResult(result);
+                            }
+                            previousHeightDiff = pixelHeightDiff;
+                         }
+                    };
+
+                    rootView.getViewTreeObserver().addOnGlobalLayoutListener(list);
+                	
+                	
+                    PluginResult dataResult = new PluginResult(PluginResult.Status.OK);
+                    dataResult.setKeepCallback(true);
+                    callbackContext.sendPluginResult(dataResult);
+                }
+            });
+            return true;
+        }
         return false;  // Returning false results in a "MethodNotFound" error.
     }
 
 
 }
+
 
