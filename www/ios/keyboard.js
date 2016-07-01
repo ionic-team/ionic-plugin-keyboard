4aefdf376c83d3671381aea4927d127a02b68bb6
Ibrahim Hadeed <ihadeed@users.noreply.github.com>
2016-07-01 15:05:21 -0400

    fix(ios): add onSuccess and onError

--- www/ios/keyboard.js
+++ www/ios/keyboard.js
@@ -7,23 +7,24 @@ var argscheck = require('cordova/argscheck'),
 var Keyboard = function() {
 };
 
-Keyboard.hideKeyboardAccessoryBar = function(hide) {
-    exec(null, null, "Keyboard", "hideKeyboardAccessoryBar", [hide]);
+Keyboard.hideKeyboardAccessoryBar = function(hide, onSuccess, onError) {
+    exec(onSuccess, onError, "Keyboard", "hideKeyboardAccessoryBar", [hide]);
 };
 
-Keyboard.close = function() {
-    exec(null, null, "Keyboard", "close", []);
+Keyboard.close = function(onSuccess, onError) {
+    exec(onSuccess, onError, "Keyboard", "close", []);
 };
 
-Keyboard.show = function() {
+Keyboard.show = function(onSuccess, onError) {
+    if(typeof onSuccess == 'function') onSuccess();
     console.warn('Showing keyboard not supported in iOS due to platform limitations.')
     console.warn('Instead, use input.focus(), and ensure that you have the following setting in your config.xml: \n');
     console.warn('    <preference name="KeyboardDisplayRequiresUserAction" value="false"/>\n');
     // exec(null, null, "Keyboard", "show", []);
 };
 
-Keyboard.disableScroll = function(disable) {
-    exec(null, null, "Keyboard", "disableScroll", [disable]);
+Keyboard.disableScroll = function(disable, onSuccess, onError) {
+    exec(onSuccess, onError, "Keyboard", "disableScroll", [disable]);
 };
 
 /*
